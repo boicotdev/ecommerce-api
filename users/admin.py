436d36ef931cb9ed7ed93a44d0310966ad212e41
boicotdev <carlos.guzman93@live.com>
2024-10-14 13:57:45 -0500

    creamos un formulario personalizado para guardar las contraseñas del usuario hasheadas cuando se crean desde el admin panel de django

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.forms import ModelForm
 from .models import User
 
 
 
-admin.site.register(User)
+# Crear un formulario para el admin para asegurar que la contraseña se maneje correctamente
+class CustomUserChangeForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        # Si se está creando un nuevo usuario o si se cambia la contraseña
+        if 'password' in self.changed_data:
+            user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
+# Personalizar el UserAdmin para incluir los campos adicionales
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('rol', 'phone', 'address')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
