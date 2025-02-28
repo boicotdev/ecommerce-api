707261ec7ddd96c6721a40d3db61e507bed7da9f
boicotdev <carlos.guzman93@live.com>
2025-02-28 04:12:08 -0500

    commit

--- build.sh
+++ build.sh
@@ -9,4 +9,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
+python manage.py makemigrations
+python manage.py makemigrations users
+python manage.py makemigrations products
 python manage.py migrate
