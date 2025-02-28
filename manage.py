c333190207851edca060b1ecc3a25a00e16dff56
boicotdev <carlos.guzman93@live.com>
2025-02-28 03:47:31 -0500

    bug

--- manage.py
+++ manage.py
@@ -1,27 +1,25 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
-import django
-from django.contrib.auth import get_user_model
 from decouple import config
-
-django.setup()
-User = get_user_model()
-
-SUPERUSER_EMAIL = config("SUPERUSER_EMAIL", default="admin@example.com")
-SUPERUSER_PASSWORD = config("SUPERUSER_PASSWORD", default="admin123")
-
-if not User.objects.filter(email=SUPERUSER_EMAIL).exists():
-    User.objects.create_superuser(username="admin", email=SUPERUSER_EMAIL, password=SUPERUSER_PASSWORD)
-    print("Superusuario creado exitosamente")
-else:
-    print("El superusuario ya existe")
-
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')  # ✅ Definir primero la configuración
+    import django
+    django.setup()  # ✅ Ahora sí puedes ejecutar setup()
+
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+
+    SUPERUSER_EMAIL = config("SUPERUSER_EMAIL", default="admin@example.com")
+    SUPERUSER_PASSWORD = config("SUPERUSER_PASSWORD", default="admin123")
+
+    if not User.objects.filter(email=SUPERUSER_EMAIL).exists():
+        User.objects.create_superuser(username="admin", email=SUPERUSER_EMAIL, password=SUPERUSER_PASSWORD)
+        print("Superusuario creado exitosamente")
+    else:
+        print("El superusuario ya existe")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
